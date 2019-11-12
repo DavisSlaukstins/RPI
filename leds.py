3c2deb402985f8265b203579b8eed3b7aae9c699
DavisSlaukstins <48949243+DavisSlaukstins@users.noreply.github.com>
2019-11-12 09:44:36 +0200

    Update leds.py

--- leds.py
+++ leds.py
@@ -1,1 +1,44 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(4,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
 
+GPIO.setup(22, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, GPIO.PUD_UP)
+
+while True:
+    button_state1 = GPIO.input(22)
+    button_state2 = GPIO.input(23)
+    button_state3 = GPIO.input(24)
+    if button_state1 == GPIO.LOW:
+        GPIO.output(4,GPIO.HIGH)
+    if button_state2 == GPIO.LOW:
+        GPIO.output(17,GPIO.HIGH)
+    if button_state3 == GPIO.LOW:
+        GPIO.output(27,GPIO.HIGH)
+    if button_state1 == GPIO.LOW and button_state2 == GPIO.LOW and button_state3 == GPIO.LOW:
+        GPIO.output(4,GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(17,GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(27,GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(4,GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(17,GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(27,GPIO.HIGH)
+        time.sleep(2)
+    else:
+        GPIO.output(4,GPIO.LOW)
+        GPIO.output(17,GPIO.LOW)
+        GPIO.output(27,GPIO.LOW)
+except KeyboardInterrupt:
+    print(counter)
+    GPIO.cleanup()
+finally:
+    GPIO.cleanup()
