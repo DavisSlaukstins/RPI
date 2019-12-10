619c71beed3e4f7a6eed7b8991ed4c3ea5c0355b
DavisSlaukstins <48949243+DavisSlaukstins@users.noreply.github.com>
2019-12-10 09:34:58 +0200

    Update web.py

--- web.py
+++ web.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
 
 
 app = Flask(__name__)
@@ -13,7 +19,17 @@ def cakes():
 
 @app.route('/hello/<name>')
 def hello(name):
+    if name == "paul"
+        GPIO.output(17,GPIO.HIGH)
+        GPIO.output(27,GPIO.HIGH)
+
     return render_template('page.html', name=name)
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
